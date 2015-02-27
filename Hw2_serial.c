e72e77c66dcafb776ab42f64dafc1505dc370f39
JERidenour <fireflightning@gmail.com>
2015-02-27 17:26:09 +0100

    improved tolerance check

--- Hw2_serial.c
+++ Hw2_serial.c
@@ -7,19 +7,12 @@
 #define h 1.0/(N+1)
 #define tol 1e-6
 
-// compare is used by qsort() to compare values in an array
-static int compare (const void * a, const void * b)
-{
-  if (*(double*)a > *(double*)b) return 1;
-  else if (*(double*)a < *(double*)b) return -1;
-  else return 0;  
-}
 
 int main(int argc, char *argv[])
 {
 
-	double f[N+1], r[N+1], u[N+1], prev_u[N+1], u_diff[N+1];
-	double max_diff = 1.0;
+	double f[N+1], r[N+1], u[N+1], prev_u[N+1];
+	double max_diff = tol*2;
 	int i;
 
 	// define function values and give initial guess for u:
@@ -33,6 +26,9 @@ int main(int argc, char *argv[])
 	u[0]=0.0;
 	u[N]=0.0;
 
+	// //maxdiff
+	// max_diff = 0.0;
+
 	while(max_diff > tol){
 
 		// save old values 
@@ -40,21 +36,18 @@ int main(int argc, char *argv[])
 			prev_u[i] = u[i];
 		}
 
+		double biggest = 0.0;
+
 		// update u
 		for(i=1; i<N; i++){
 			u[i] = (u[i-1] + u[i+1] - h*h*f[i])/(2-h*h*r[i]);
+			double current_diff = fabs(u[i] - prev_u[i]);
+			if(current_diff > biggest){
+				biggest = current_diff;
+			}
 		}
 
-		// get the difference between iterations
-		for (i=0; i<N+1; i++){
-			u_diff[i] = fabs(u[i]-prev_u[i]);
-		}		
-
-		// sort u_diff to find maximum value
-		qsort(u_diff, N+1, sizeof(u_diff[0]), compare);
-
-		// set max value to check against tolerance
-		max_diff = u_diff[N];
+		max_diff = biggest;
 	}
 
     // write to file
